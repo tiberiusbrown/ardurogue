c1c591abaa14d2f72f16f3b2fdcb6f36ddb037be
Peter Brown <brow1067@gmail.com>
2022-04-28 23:00:35 -0600

    try again with the table

--- tools/tables.cpp
+++ tools/tables.cpp
@@ -17,14 +17,14 @@ int main()
         int max_att = 20;
         int max_def = 20;
         printf("<table>\n");
-        printf("<tr><td /><td colspan=\"%d\" style=\"text-align:center\"><b>Defense</b></td></tr>\n", max_def + 1);
-        printf("<tr style=\"text-align:right\"><td><b>Attack</b></td>");
+        printf("<tr><td /><td colspan=\"%d\" align=\"center\"><b>Defense</b></td></tr>\n", max_def + 1);
+        printf("<tr align=\"right\"><td><b>Attack</b></td>");
         for(int def = 0; def <= max_def; ++def)
             printf("<td><b>%d</b></td>", def);
         printf("</tr>\n");
         for(int att = 0; att <= max_att; ++att)
         {
-            printf("<tr style=\"text-align:right\"><td><b>%d</b></td>", att);
+            printf("<tr align=\"right\"><td><b>%d</b></td>", att);
             for(int def = 0; def <= max_def; ++def)
             {
                 int dam = calc_hit_dam_post_mod((uint8_t)att, (uint8_t)def);
